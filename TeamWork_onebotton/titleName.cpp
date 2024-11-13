33f86256050ba04c78c7fdeb364b4bd6c070ca67
kusama07 <115055012+kusama07@users.noreply.github.com>
2024-11-13 16:59:09 +0900

    とちゅつ

--- TeamWork_onebotton/titleName.cpp
+++ TeamWork_onebotton/titleName.cpp
@@ -21,9 +21,7 @@ void titleName::Draw()
 {
 	for (int i = 0; i < map.size();i++) {
 		for (int j = 0; j < map[i].size();j++) {
-			if (map[j][i] == 1) {
 				Novice::DrawBox(j * width_, i * width_, width_, width_, 0.0f, WHITE, kFillModeSolid);
-			}
 		}
 	}
 }
