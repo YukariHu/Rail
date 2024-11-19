1d125cfe1e8327fd801a51424bc3fd649650b884
kusama07 <115055012+kusama07@users.noreply.github.com>
2024-11-19 10:51:04 +0900

    途中

--- TeamWork_onebotton/fireFlower.h
+++ TeamWork_onebotton/fireFlower.h
@@ -6,7 +6,7 @@ public:
     FireFlower();
     ~FireFlower();
 
-    void Init(const Vector2& direction);
+    void Init(Vector2 direction, float xPosition, float yPosition);
     void Update();
     void Draw();
     bool IsAlive() const;
@@ -16,6 +16,7 @@ private:
     Vector2 direction_;
     float rad_;
     float velocity_;
-    float oscillation_;
+    float expandRate_;
+    int time_;
     bool alive_;
 };
