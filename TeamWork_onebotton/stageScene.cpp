2274f7b9e58e56c4d3073342cf242dafa81b1711
kusama07 <115055012+kusama07@users.noreply.github.com>
2024-11-26 15:27:39 +0900

    Revert "Revert "ダッシュとUI""
    This reverts commit c50b1469a28a62f319ddde6ed65f2bf9878a1170.

--- TeamWork_onebotton/stageScene.cpp
+++ TeamWork_onebotton/stageScene.cpp
@@ -25,8 +25,13 @@ StageScene::StageScene(int _id)
 	bossHpBarPos = { 5.0f,0.0f };
 	bossHpBarSize = { windowWidth - 10.0f,20.0f };
 
-	playerHpBarPos = { 30.0f,windowHeight - 30.0f };
+	playerHpBarPos = { 30.0f,windowHeight - 50.0f };
 	playerHpBarSize = { 120.0f,15.0f };
+
+	dashBarPos = { 25.0f,windowHeight - 30.0f };
+	dashBarSize = { 180.0f,15.0f };
+
+	
 }
 
 StageScene::~StageScene()
@@ -40,8 +45,11 @@ void StageScene::onEnter()
 	player = new Player();
 	bossHpBar = new Bar(bossHpBarPos,bossHpBarSize,boss->GetHp(), GetColor(255, 48, 48, 255), WHITE,4);//  255, 106, 106
 	playerHpBar = new Bar(playerHpBarPos, playerHpBarSize, player->GetHp(), GetColor(0, 205,102, 255));//0, 255,127
-
+	
+	dashBar = new Bar(dashBarPos, dashBarSize, dynamic_cast<Player*>(player)->GetDashCount(), GetColor(255,215, 0, 255), GetColor(0, 205, 102, 000),1);//0, 255,127
+	
 	boss->SetTarget(player);
+	//player.ge
 
 
 	for (int i = 0; i < 20; ++i) {
@@ -63,7 +71,7 @@ void StageScene::onInput(char* keys, char* prekeys)
 
 	if (keys[DIK_SPACE] && !prekeys[DIK_SPACE])
 	{
-		mainCamera.Shack(4,0.3f);
+		//mainCamera.Shack(4,0.3f);
 	}
 }
 
@@ -74,8 +82,11 @@ void StageScene::update()
 	boss->onUpdate();
 	player->onUpdate();
 	
+
+	//*********bar
 	bossHpBar->Update(boss->GetHp());
 	playerHpBar->Update(player->GetHp());
+	dashBar->Update(dynamic_cast<Player*>(player)->GetDashCount());
 
 	//**********particle
 	backParticle.Update();
@@ -99,6 +110,7 @@ void StageScene::draw(const Camera& camera)
 	
 	bossHpBar->Draw();
 	playerHpBar->Draw();
+	dashBar->Draw();
 
 	Novice::ScreenPrintf(0, 0, "PlayerHP:%d", player->GetHp());
 	Novice::ScreenPrintf(0, 20, "BossHP:%d", boss->GetHp());
@@ -117,6 +129,8 @@ void StageScene::onExit()
 	delete playerHpBar;
 	playerHpBar = nullptr;
 
+	delete dashBar;
+	dashBar = nullptr;
 
 
 	for (auto bullet : bulletList)
