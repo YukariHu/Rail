30eb1b3f774a594e771c9b8f1b84b65ed7f1fae9
kusama07 <115055012+kusama07@users.noreply.github.com>
2024-11-26 15:50:37 +0900

    a

--- TeamWork_onebotton/fireFlower.cpp
+++ TeamWork_onebotton/fireFlower.cpp
@@ -1,7 +1,10 @@
 #include "FireFlower.h"
+#include <cmath>
+#include <cstdlib>
+#include <Novice.h>
 
 FireFlower::FireFlower() :
-	pos_({ 0, 0 }), direction_({ -1.0f, 0.3f }), rad_(4.0f), velocity_(7.0f), oscillation_(0.0f), alive_(true)
+    pos_({ 0, 0 }), direction_({ 0.0f, 0.0f }), rad_(10.0f), velocity_(2.0f), time_(120), alive_(true), expandRate_(0.0f)
 {
 }
 
@@ -9,20 +12,42 @@ FireFlower::~FireFlower()
 {
 }
 
-void FireFlower::Init(const Vector2& direction)
+void FireFlower::Init(Vector2 direction, float xPosition, float yPosition)
 {
-	pos_ = {direction};
+    pos_ = { xPosition, yPosition };
+    time_ = 120;
+    alive_ = true;
+    rad_ = 10.0f;
+
+    direction_ = { direction.x, direction.y };
+    expandRate_ = 0.0f;
 }
 
 void FireFlower::Update()
 {
+    float progress = static_cast<float>(time_) / 30.0f;
+    expandRate_ = 1.0f - (1.0f - progress) * (1.0f - progress);
+
+    pos_.x += direction_.x * velocity_ * expandRate_;
+    pos_.y += direction_.y * velocity_ * expandRate_;
+
+    rad_ -= 0.1f;
+    if (rad_ < 0) rad_ = 0;
+
+    time_--;
+    if (time_ <= 0) {
+        alive_ = false;
+    }
 }
 
 void FireFlower::Draw()
 {
+    if (alive_) {
+        Novice::DrawEllipse((int)pos_.x, (int)pos_.y, (int)rad_, (int)rad_, 0.0f, 0xB6BBC4FF, kFillModeSolid);
+    }
 }
 
 bool FireFlower::IsAlive() const
 {
-	return false;
+    return alive_;
 }
