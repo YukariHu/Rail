6413b2ad15228a613499059756577b5fc832ebc2
kusama07 <115055012+kusama07@users.noreply.github.com>
2024-11-26 15:23:27 +0900

    Revert "途中"
    This reverts commit 1d125cfe1e8327fd801a51424bc3fd649650b884.

--- TeamWork_onebotton/ClearScene.h
+++ TeamWork_onebotton/ClearScene.h
@@ -1,19 +1,18 @@
 #pragma once
 #include <Scene.h>
-#include "FireFlowerManager.h"
 
 class ClearScene : public Scene
 {
-public:
-    ClearScene() = default;
-    ~ClearScene() = default;
+	public:
+	ClearScene() = default;
+	~ClearScene() = default;
 
-    void onEnter() override;
-    void onInput(char* keys, char* prekeys) override;
-    void update() override;
-    void draw() override;
+	void onEnter() override;
+	void onInput(char* keys,char* prekeys) override;
+	void update() override;
+	void draw() override;
+	//void onExit() override;
 
-private:
-    FireFlowerManager fireFlower_;
-    int fireFlowerTime_;
+	private:
+
 };
