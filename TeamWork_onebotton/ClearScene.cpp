b24badfe7569688b7658fdca18ccd5000f9bdd2d
kusama07 <115055012+kusama07@users.noreply.github.com>
2024-11-26 15:28:08 +0900

    Revert "Revert "途中""
    This reverts commit 6413b2ad15228a613499059756577b5fc832ebc2.

--- TeamWork_onebotton/ClearScene.cpp
+++ TeamWork_onebotton/ClearScene.cpp
@@ -1,53 +1,60 @@
-#pragma once
 #include "ClearScene.h"
 #include <SceneManager.h>
 #include <Novice.h>
 
-extern int stageSelectBackground;
 extern SceneManager sceneManager;
 
 void ClearScene::onEnter()
 {
+    fireFlower_.Init();
+    fireFlowerTime_ = 180;
 }
 
 void ClearScene::onInput(char* keys, char* prekeys)
 {
-	if (keys[DIK_BACK] && !prekeys[DIK_BACK])
-	{
-		sceneManager.switchScene(SceneManager::SceneType::Title);
-	}
+    if (keys[DIK_BACK] && !prekeys[DIK_BACK])
+    {
+        sceneManager.switchScene(SceneManager::SceneType::Title);
+    }
 
-	if (keys[DIK_RIGHT] && !prekeys[DIK_RIGHT])
-	{
-		if (sceneManager.selectedStage < sceneManager.stageNum - 1)
-		{
-			sceneManager.selectedStage++;
-		}
-	}
-	if (keys[DIK_LEFT] && !prekeys[DIK_LEFT])
-	{
-		if (sceneManager.selectedStage > 0) 
-		{
-			sceneManager.selectedStage--;
-		}
-	}
-	if (keys[DIK_SPACE] && !prekeys[DIK_SPACE])
-	{
-		sceneManager.switchScene(SceneManager::SceneType::Game);
-	}
+    if (keys[DIK_RIGHT] && !prekeys[DIK_RIGHT])
+    {
+        if (sceneManager.selectedStage < sceneManager.stageNum - 1)
+        {
+            sceneManager.selectedStage++;
+        }
+    }
+    if (keys[DIK_LEFT] && !prekeys[DIK_LEFT])
+    {
+        if (sceneManager.selectedStage > 0)
+        {
+            sceneManager.selectedStage--;
+        }
+    }
+    if (keys[DIK_SPACE] && !prekeys[DIK_SPACE])
+    {
+        sceneManager.switchScene(SceneManager::SceneType::Game);
+    }
 }
 
 void ClearScene::update()
 {
+    fireFlower_.Update();
+    fireFlowerTime_--;
+
+    if (fireFlowerTime_ <= 0) {
+        fireFlower_.Create();
+        fireFlowerTime_ = 180;
+    }
 }
 
 void ClearScene::draw()
 {
-	Novice::DrawBox(0, 0, 1280, 720, 0.0f, 0xCAC9A1FF, kFillModeSolid);
+    Novice::DrawBox(0, 0, 1280, 720, 0.0f, 0x333333FF, kFillModeSolid);
+    fireFlower_.Draw();
 
-	Novice::DrawLine(0, 720 / 2, 1280, 720 / 2, 0x4A505FFF);
+    Novice::DrawLine(0, 720 / 2, 1280, 720 / 2, 0x4A505FFF);
+    Novice::DrawEllipse(1280 / 2, 720 / 2, 20, 20, 0.0f, 0x4A505FFF, kFillModeSolid);
 
-	Novice::DrawEllipse(1280 / 2, 720 / 2, 20, 20, 0.0f, 0x4A505FFF, kFillModeSolid);
-
-	Novice::ScreenPrintf(0,0,"select:-> <- stageId %d", sceneManager.selectedStage);
+    Novice::ScreenPrintf(0, 0, "select:-> <- stageId %d", sceneManager.selectedStage);
 }
--- TeamWork_onebotton/ClearScene.h
+++ TeamWork_onebotton/ClearScene.h
@@ -1,18 +1,19 @@
 #pragma once
 #include <Scene.h>
+#include "FireFlowerManager.h"
 
 class ClearScene : public Scene
 {
-	public:
-	ClearScene() = default;
-	~ClearScene() = default;
+public:
+    ClearScene() = default;
+    ~ClearScene() = default;
 
-	void onEnter() override;
-	void onInput(char* keys,char* prekeys) override;
-	void update() override;
-	void draw() override;
-	//void onExit() override;
+    void onEnter() override;
+    void onInput(char* keys, char* prekeys) override;
+    void update() override;
+    void draw() override;
 
-	private:
-
+private:
+    FireFlowerManager fireFlower_;
+    int fireFlowerTime_;
 };
